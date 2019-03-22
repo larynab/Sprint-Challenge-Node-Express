eb3c9ea230db6ac8c47593e234c67b6e3f50e37a
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-03-22 09:31:11 -0700

    posting-posts

--- data/helpers/base-router.js
+++ data/helpers/base-router.js
@@ -7,7 +7,7 @@ const projectModel = require("./projectModel.js");
 const router = express.Router();
 
 //ACTIONS CRUD
-
+//GET
 router.get("/", (req, res) => {
   actionModel
     .get()
@@ -18,7 +18,7 @@ router.get("/", (req, res) => {
       res.status(500).json({ error: "Error retrieving actions" });
     });
 });
-
+//GET BY ID
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   actionModel
@@ -27,12 +27,29 @@ router.get("/:id", (req, res) => {
       res.status(200).json(actions);
     })
     .catch(err => {
-      res.status(500).json({ errorMessage: "There was an error" });
+      res
+        .status(500)
+        .json({ errorMessage: "There was an error getting action" });
     });
 });
 
-//PROJECTS CRUD
+//POST
+router.post("/", (req, res) => {
+  let newAction = req.body;
 
+  if (!newAction.description && !newAction.notes) {
+    res
+      .status(400)
+      .json({ errorMessage: "Error, you need description and notes, please" });
+  } else {
+    actionModel.insert(newAction).then(actions => {
+      res.status(201).json(actions.id);
+    });
+  }
+});
+
+//PROJECTS CRUD
+//GET
 router.get("/", (req, res) => {
   projectModel
     .get()
@@ -42,6 +59,34 @@ router.get("/", (req, res) => {
     .catch(error => {
       res.status(500).json({ error: "Error retrieving projects" });
     });
+});
+//GET BY ID
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  projectModel
+    .get(id)
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessage: "There was an error getting project" });
+    });
+});
+//POST
+router.post("/", (req, res) => {
+  let newProject = req.body;
+
+  if (!newProject.description && !newProject.name) {
+    res
+      .status(400)
+      .json({ errorMessage: "Need description and notes!!!!!!!!" });
+  } else {
+    projectModel.insert(newProject).then(post => {
+      res.status(201).json(post.id);
+    });
+  }
 });
 
 module.exports = router;
